6df55529ff2c74469f1445a3dac2217b9c797b4b
afrahjarallh9-max <afrahjarallh9@gmail.com>
2026-08-17 12:39:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ db.serialize(()=>{
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(__dirname));
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -149,5 +149,5 @@ app.get("/api/records/:id/recipients",auth,admin,async(req,res)=>{
     FROM recipients x JOIN users u ON u.id=x.user_id WHERE x.record_id=? ORDER BY u.name`,[req.params.id]));
 });
 
-app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")));
+app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"index.html")));
 app.listen(PORT,()=>console.log("School Admin running on "+PORT));
